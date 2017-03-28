100ca9a23bd75e4ae56b4bfce2bbbce96830f662
BuildTools <unconfigured@null.spigotmc.org>
2017-03-28 23:08:05 +0300

    Init commit

---
+++ src/aas_displayer/main.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # TODO: ADD ARGS
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aas_displayer",
+    version="0.0.1",
+    author="Matan Merom",
+    description=("A demonstration of how to load an AAS file and print the dialogue texts to the screen on the right "
+                 "time."),
+    packages=find_packages(),
+)
