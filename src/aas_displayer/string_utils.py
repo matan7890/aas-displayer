12d4a744c27a06bc58ed125c34ef3d5767d45970
matan7890 <matan7890@gmail.com>
2017-04-01 15:21:10 +0300

    Added some useful utils on strings.

---
+++ src/aas_displayer/string_utils.py
@@ -0,0 +1,31 @@
+import re
+import datetime as dt
+
+
+def camel_case_to_snake_case(camel_string):
+    """
+    This function converts CamelCase strings to snake_case strings.
+    Taken from: https://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-snake-case
+
+    :param camel_string: the camel cased string that needs to be converted to snake_string
+    :type camel_string: str
+    :return: a snake_cased string.
+    :rtype: str
+    """
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', camel_string)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def convert_time_to_milli_seconds(time_str):
+    """
+    This function converts time str in the format of Hrs:Mins:Secs:hundredths to the amount of milliseconds passed since
+    00:00:00:00.
+
+    :param time_str: a time str with the format of Hrs:Mins:Secs:hundredths
+    :type time_str: str
+    :return: the amount of milliseconds passed since 00:00:00:00 this str represents as a time object.
+    :rtype: dt.time
+    """
+    hours, minutes, seconds, hundredth = map(int, time_str.split(':'))
+    microseconds = hundredth * 10000
+    return dt.time(hours, minutes, seconds, microseconds)
