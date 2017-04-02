384b57691d90a596bb951dcfc01b2ef23dae6856
matan7890 <matan7890@gmail.com>
2017-04-02 21:43:11 +0300

    Now importing all events files in the events init.

--- src/aas_displayer/parsers/events/__init__.py
+++ src/aas_displayer/parsers/events/__init__.py
@@ -1,1 +1,6 @@
-from dialogue import Dialogue
+from __future__ import absolute_import
+
+from .dialogue import Dialogue
+from .comment import Comment
+from .event import Event
+from .parser import EventParser
